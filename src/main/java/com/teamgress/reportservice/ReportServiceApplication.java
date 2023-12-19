e31af8aa8d9e0251368f0cab18145c39025073ce
BabisAmas <babis_ser@hotmail.com>
2023-12-19 23:28:33 +0200

    initializes report service project

---
+++ src/main/java/com/teamgress/reportservice/ReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teamgress.reportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportServiceApplication.class, args);
+	}
+
+}
